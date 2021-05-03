3a2b71cea669dd3b08405d4fd19e195e3284ca71
kapilbhanushali3 <47662487+kapilbhanushali3@users.noreply.github.com>
2021-05-03 14:35:09 +0530

    ajax api

---
+++ villa.php
@@ -0,0 +1,21 @@
+<?php
+
+$page = $_GET['pgno'];
+function httpGet($url)
+{
+    $ch = curl_init();  
+ 
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+//  curl_setopt($ch,CURLOPT_HEADER, false); 
+ 
+    $output=curl_exec($ch);
+ 
+    curl_close($ch);
+    return $output;
+}
+ 
+echo httpGet("http://www.pinkvilla.com/photo-gallery-feed-page/page/".$page);
+
+
+?>
